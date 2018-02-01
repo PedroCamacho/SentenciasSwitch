31213e82d2b423d16b8dc6608f1ed8767ee28fc0
Pedro <Pedro@Pedro-Desktop>
2018-02-01 11:09:19 +0100

    Root Commit

---
+++ src/EjemploSwitch.java
@@ -0,0 +1,15 @@
+
+public class EjemploSwitch {
+
+	public static void main(String[] args) {
+		int j = 5;
+		switch (j) {
+		case 5:
+			System.out.println("Caso 5");
+			break;
+		case 4:
+			System.out.println("Caso 5");
+			break;
+		}
+	}
+}
