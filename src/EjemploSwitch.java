ccac066adcffee8c78243faf05ad93faa78d1d54
pedro.camacho <pedro.camacho@gmail.com>
2018-10-16 11:41:02 +0200

    Código completo del inicio del vídeo

--- src/EjemploSwitch.java
+++ src/EjemploSwitch.java
@@ -2,14 +2,22 @@
 public class EjemploSwitch {
 
 	public static void main(String[] args) {
-		int j = 5;
+		int j = 0;
 		switch (j) {
 		case 5:
 			System.out.println("Caso 5");
 			break;
 		case 4:
-			System.out.println("Caso 5");
+			System.out.println("Caso 4");
 			break;
+		case 3:
+			System.out.println("Caso 3");
+		case 2:
+			System.out.println("Caso 2");
+		case 1:
+			System.out.println("Caso 1");
+		default:
+			System.out.println("Por defecto");
 		}
 	}
 }
